c97af75499b39ad05e6cafc9fab4bf292a8158e3
Aanak30 <takanksha455@gmai.com>
2026-04-09 23:48:15 +0530

    add Routing

---
+++ src/routes/Route.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../pages/Home";
+import ProductList from "../pages/ProductList";   // ✅ ADD THIS
+import ProductDetail from "../pages/ProductDetail";
+import Cart from "../pages/Cart";
+import Checkout from "../pages/Checkout";
+import NotFound from "../pages/NotFound";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "products",
+        element: <ProductList />   // ✅ NOW WORKS
+      },
+      {
+        path: "product/:id",
+        element: <ProductDetail />
+      },
+      {
+        path: "cart",
+        element: <Cart />
+      },
+      {
+        path: "checkout",
+        element: <Checkout />
+      }
+    ]
+  }
+]);
